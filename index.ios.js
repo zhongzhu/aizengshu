7fce6be625c0bde817cd06056c4d3557df01f9e7
zhongzhu <zhongzhu@ymail.com>
2016-08-18 22:31:02 +0800

    learning TabBarIOS

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,9 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * 爱赠书：断舍离的时候把旧书扔掉，倒不如看看或许别人正好需要这本书？
+ *
+ * https://github.com/zhongzhu/aizengshu
+ *
+ * @zhongzhu
  */
 
 import React, { Component } from 'react';
@@ -9,25 +11,55 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TabBarIOS
 } from 'react-native';
 
+import Book from './ios_views/bookList';
+
 class aizengshu extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {selectedTab: '图书'};
+  }
+
+  _renderContent(text) {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>{text}</Text>
       </View>
     );
+  }
+
+  render() {
+    return (
+      <TabBarIOS>
+        <TabBarIOS.Item
+          title='图书'
+          selected={this.state.selectedTab === '图书'}
+          onPress={() => {
+            this.setState({ selectedTab: '图书' });
+          }}>
+          <Book title='爱赠书'/>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title='查找'
+          selected={this.state.selectedTab === '查找'}
+          onPress={() => {
+            this.setState({ selectedTab: '查找' });
+          }}>
+          <Book title='爱查找'/>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title='我'
+          selected={this.state.selectedTab === '我'}
+          onPress={() => {
+            this.setState({ selectedTab: '我' });
+          }}>
+          <Book title='爱我'/>
+        </TabBarIOS.Item>                
+      </TabBarIOS>
+    )
   }
 }
 
